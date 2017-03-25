bdd2cabb78b4db744f911adfba5f935bb30f59a2
LuD1161 <aseemshrey@gmail.com>
2017-03-25 11:45:59 +0530

    Added logout endpoint

--- HINT17/aparoksha/urls.py
+++ HINT17/aparoksha/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     url(r'^renderLogin', views.renderLogin),
     url(r'^renderNotification', views.renderNotification),
     url(r'^login', views.login),
-    url(r'^getreq', views.getrequest),
     url(r'^donate', views.donate),
     url(r'^signup$', views.signup),
     url(r'^rendersignup/$', views.renderSignup),
