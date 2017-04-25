302da4ba9393ac993ca5eaac4a74c3cd3f91dfa8
Anurag kushwaha <kanurag078@gmail.com>
2017-04-26 03:13:17 +0530

    You know nothing Anurag
    THIS

--- HINT17/app/components/profile.js
+++ HINT17/app/components/profile.js
@@ -1,81 +1,95 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
 import {
     ListView,
     Text,
     View,
     Image,
-TouchableOpacity
+    FlatList
 } from 'react-native';
-var RNFS = require('react-native-fs');
-import { Card } from './card';
-import { CardSection } from './cardSection';
-import { connect } from 'react-redux';
-import axios from 'axios';
 
-var value = '';
-    var arrr= [];
+import {
+    connect
+} from 'react-redux';
+import axios from 'axios';
+import {
+    Card
+} from './card';
+import {
+    CardSection
+} from './cardSection';
+import {
+    fetchData
+} from '../actions';
+
+
+
 class showProfile extends Component {
 
-  getMoviesFromApiAsync() {
-    //  fetch('http://35.166.45.231:8080/notifications?user_email=anurag@anurag.com')
-    //   .then((response) => {
-    //     return response.json();
-    //   })
-    // .then((dataa) => this.setState({
+    componentDidMount() {
+        this.props.fetchData(this.props.email);
+        this.ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+    }
 
-    //       weather: dataa.data
-    //     }))
-
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
-
-  }
-    componentWillMount() {
-    // this.state = {
-    //   weather: 'c'
-    // }//object
-    //     axios.get('http://35.166.45.231:8080/notifications?user_email=anurag@anurag.com')
-    //         .then((responsed) => responsed)
-    //       .then(response => this.state = {
-    //           ...this.state,
-    //           weather: 'chutiya'
-    //         }).done();
-    //     // console.warn(daa.then((response) => arrr.push(response)));
-    //     console.log(this.state.weather)
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows(this.props.pList),
-    };
-
+    componentDidUpdate() {
+      console.log(typeof this.props.pList);
     }
 
     renderRow(data) {
-        return (
-        <Card>
-            <CardSection><Text>{data.from}                                        {data.status}</Text></CardSection>
-            <CardSection><Text>{data.date} </Text></CardSection>
-        </Card>
-            );
-        }
+        return ( 
+          <Card>
+            <Text>{data.date} </Text> 
+          </Card>
+        );
+    }
 
 
     render() {
-        return (
-    <View style={styles.vcontainer}>
-      <View style={styles.container}>
-        <Text style={styles.textStyle1}>
-          <Image style={styles.image} source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/User_Circle.png'}} />
-          <Text style={styles.textStyle2}>        Anurag Kushwaha</Text> 
-        </Text>
-      </View>
+      console.log(this.props.pList);
+        if (this.props.pList) {
+            return ( 
+              <View 
+                style={
+                    styles.vcontainer
+                }>
 
-        <ListView
-            dataSource={this.state.dataSource}
-            renderRow={this.renderRow}
-         />
-    </View>
+                <View 
+                  style={
+                    styles.container
+                } >
+                <Text 
+                  style={
+                    styles.textStyle1
+                } >
+                <Image 
+                  style={
+                    styles.image
+                }
+                source={
+                    {
+                        uri: 'https://upload.wikimedia.org/wikipedia/commons/d/d3/User_Circle.png'
+                    }
+                }
+                /> <Text style = {
+                    styles.textStyle2
+                } > Anurag Kushwaha </Text>  </Text>
+                 </View> 
+
+                <ListView dataSource={this.ds.cloneWithRows(this.props.pList)}
+                renderRow={this.renderRow.bind(this)}
+                />
+              </View>
             );
+        }
+        return ( 
+          <View >
+            <Text >
+            Coming soon </Text>
+          </View>
+        );
     }
 }
 
@@ -83,32 +97,36 @@ const styles = {
     vcontainer: {
         backgroundColor: 'grey'
     },
-  container: {
-    backgroundColor: 'black',
-    marginTop: 0.1,
-    marginHorizontal: 0.1,
-  },
-  image: {
-    width: 200,
-    height: 200,
-  },
-  textStyle1: {
-    fontSize: 18,
-    padding: 15,
-    backgroundColor: 'transparent'
-  },
-  textStyle2: {
-    fontSize: 18,
-    padding: 15,
-    color: 'white',
-    backgroundColor: 'transparent'
-  }
+    container: {
+        backgroundColor: 'black',
+        marginTop: 0.1,
+        marginHorizontal: 0.1,
+    },
+    image: {
+        width: 200,
+        height: 200,
+    },
+    textStyle1: {
+        fontSize: 18,
+        padding: 15,
+        backgroundColor: 'transparent'
+    },
+    textStyle2: {
+        fontSize: 18,
+        padding: 15,
+        color: 'white',
+        backgroundColor: 'transparent'
+    }
 };
 
 const mapStateToProps = state => {
+    //console.log( state.auth);
     return {
-        pList: state.dat
-    }; 
+        pList: state.auth.pData,
+        email: state.auth.email
+    };
 };
 
-export default connect(mapStateToProps)(showProfile);
+export default connect(mapStateToProps, {
+    fetchData
+})(showProfile);
